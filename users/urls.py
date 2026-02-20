ebf03b0e51b0738f5615c1bd8b5e904a35879d36
Sergey Korobeynikov <skoro@smn35.com>
2026-02-20 19:15:59 +0300

    namespace="users" added to "users/" path password reset paths fix

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import views
 from .views import CustomLogoutView, ActivateUserView
@@ -12,33 +12,28 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.ActivateUserView.as_view(), name='activate'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='users/password_reset_form.html'), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="users/password_reset.html",
+            email_template_name="users/password_reset_email.html",
+            success_url=reverse_lazy("users:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html',
+        success_url=reverse_lazy("users:password_reset_complete"),
+        ),
+        name='password_reset_confirm'
+    ),
+
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
-
-# path(
-#     "password_reset/",
-#     auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
-#     name="password_reset"
-# ),
-# path(
-#     "password_reset/done/",
-#     auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
-#     name="password_reset_done"
-# ),
-# path(
-#     "reset/<uidb64>/<token>/",
-#     auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
-#     name="password_reset_confirm"
-# ),
-# path(
-#     "reset/done/",
-#     auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
-#     name="password_reset_complete"
-# ),
