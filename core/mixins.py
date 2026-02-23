2f41cfc1bbebcfd052f875f221863eebda6f31e7
Sergey Korobeynikov <skoro@smn35.com>
2026-02-23 22:40:31 +0300

    OwnerOrManagerMixin added

--- core/mixins.py
+++ core/mixins.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import PermissionDenied
+from django.db.models import QuerySet
 from django.http import HttpResponseForbidden
 
 
@@ -31,3 +33,38 @@ class OwnerEditMixin:
             return HttpResponseForbidden()
 
         return super().dispatch(request, *args, **kwargs)
+
+
+class OwnerOrManagerMixin:
+    """
+    - Менеджер (с нужным permission) видит все объекты
+    - Обычный пользователь видит только свои
+    - Проверяет доступ к конкретному объекту
+    """
+
+    owner_field = "owner"              # имя поля владельца
+    manager_permission = None          # например: "core.view_all_newsletters"
+
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        user = self.request.user
+
+        if not user.is_authenticated:
+            return queryset.none()
+
+        if self.manager_permission and user.has_perm(self.manager_permission):
+            return queryset
+
+        return queryset.filter(**{self.owner_field: user})
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+
+        if self.manager_permission and user.has_perm(self.manager_permission):
+            return obj
+
+        if getattr(obj, self.owner_field) != user:
+            raise PermissionDenied("У вас нет доступа к этому объекту.")
+
+        return obj
