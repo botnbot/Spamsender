d3aa80faa820d106f1e00051fad9ef5b4fd94bf9
Sergey Korobeynikov <skoro@smn35.com>
2026-02-27 18:57:20 +0300

    bformatted

--- core/urls.py
+++ core/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('newsletter/<int:pk>/delete/', NewsletterDeleteView.as_view(), name='newsletter_delete'),
     path("newsletter/<int:pk>/send/", NewsletterManualSendView.as_view(), name="newsletter_manual_send"),
     path("newsletter/<int:pk>/disable/", DisableNewsletterView.as_view(), name="newsletter_disable"),
-    path("newsletter/<int:pk>/toggle/",NewsletterToggleView.as_view(),name="newsletter_toggle"),
+    path("newsletter/<int:pk>/toggle/", NewsletterToggleView.as_view(), name="newsletter_toggle"),
 
     path("attempts/", SendAttemptListView.as_view(), name="attempt_list"),
     path("attempts/<int:pk>/", SendAttemptDetailView.as_view(), name="attempt_detail"),
