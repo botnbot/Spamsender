e60742e9075bd2ff0da79d9bd97588b807e89c47
Sergey Korobeynikov <skoro@smn35.com>
2026-03-01 14:59:04 +0300

    from django.contrib import messages get_queryset methode added to OwnerOrManagerMixin

--- core/mixins.py
+++ core/mixins.py
@@ -1,7 +1,6 @@
-from django.contrib import messages
 from django.core.exceptions import PermissionDenied
+from django.contrib import messages
 from django.shortcuts import redirect
-from django.views.generic import ListView
 
 
 class UserBlockMixin:
@@ -14,62 +13,8 @@ class UserBlockMixin:
             raise PermissionDenied("Суперпользователя нельзя заблокировать.")
         if user.groups.filter(name="Managers").exists():
             raise PermissionDenied("Менеджера нельзя заблокировать.")
-
         user.is_active = False
         user.save(update_fields=["is_active"])
-
-
-# class RoleAccessMixin:
-#     owner_field = "owner"
-#     manager_group = "Managers"
-#     no_access_redirect = "core:home"  # куда редиректим
-#
-#     def is_manager(self, user):
-#         return user.groups.filter(name=self.manager_group).exists()
-#
-#     def is_admin(self, user):
-#         return user.is_superuser
-#
-#     def get_object(self, queryset=None):
-#         try:
-#             obj = super().get_object(queryset)
-#         except Http404:
-#             # Если объекта нет — редирект с сообщением
-#             messages.error(self.request, "Нет доступа к объекту или он не существует.")
-#             return redirect(self.no_access_redirect)
-#
-#         user = self.request.user
-#
-#         if self.is_admin(user):
-#             return obj
-#
-#         if self.is_manager(user):
-#             if self.request.method in ("GET",):
-#                 return obj
-#             if getattr(obj, self.owner_field) != user:
-#                 messages.error(self.request, "Нет доступа к редактированию чужого объекта.")
-#                 return redirect(self.no_access_redirect)
-#             return obj
-#
-#         if getattr(obj, self.owner_field) != user:
-#             messages.error(self.request, "Нет доступа к этому объекту.")
-#             return redirect(self.no_access_redirect)
-#
-#         return obj
-
-
-# class RedirectOnNoAccessMixin:
-#     """
-#     Редирект с сообщением при отсутствии доступа или при 404.
-#     """
-#     no_access_redirect = "core:home"  # куда редиректить
-#
-#     def dispatch(self, request, *args, **kwargs):
-#         try:
-#             return super().dispatch(request, *args, **kwargs)
-#         except (PermissionDenied, Http404):
-#             messages.error(request, "Нет доступа к объекту или он не существует.")
-#             return redirect(self.no_access_redirect)
 
 
 class OwnerOrManagerMixin:
@@ -92,14 +37,13 @@ class OwnerOrManagerMixin:
             return True
         return False
 
+    def get_queryset(self):
         qs = super().get_queryset()
 
-        if isinstance(self, ListView):
-            if self._has_full_access():
-                return qs
-            return qs.filter(**{self.owner_field: self.request.user})
+        if self._has_full_access():
+            return qs
 
-        return qs
+        return qs.filter(**{self.owner_field: self.request.user})
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
